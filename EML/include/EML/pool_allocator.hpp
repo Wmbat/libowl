75af502836036ddc2a76fc9990a39d9d532b354a
wmbat <wmbat@protonmail.com>
2020-02-01 14:14:56 -0500

    Attempted to remove Cmake warning

--- EML/include/EML/pool_allocator.hpp
+++ EML/include/EML/pool_allocator.hpp
@@ -5,6 +5,7 @@
 
 #include <cassert>
 #include <cstdint>
+#include <memory>
 
 namespace EML
 {
@@ -18,12 +19,6 @@ namespace EML
 
    public:
       pool_allocator( std::size_t const block_count, std::size_t const block_size ) noexcept;
-      pool_allocator( pool_allocator const& other ) noexcept = delete;
-      pool_allocator( pool_allocator&& other ) noexcept;
-      ~pool_allocator( ) noexcept;
-
-      pool_allocator& operator=( pool_allocator const& rhs ) noexcept = delete;
-      pool_allocator& operator=( pool_allocator&& rhs ) noexcept;
 
       [[nodiscard]] std::byte* allocate( std::size_t size, std::size_t alignment ) noexcept override;
       void free( std::byte* p_location ) noexcept override;
@@ -34,7 +29,7 @@ namespace EML
       std::size_t block_count = 0;
       std::size_t block_size = 0;
 
-      std::byte* p_memory = nullptr;
+      std::unique_ptr<std::byte[]> p_memory;
       header* p_first_free = nullptr;
    };
 } // namespace EML
--- EML/source/EML/pool_allocator.cpp
+++ EML/source/EML/pool_allocator.cpp
@@ -6,59 +6,23 @@ namespace EML
 {
    pool_allocator::pool_allocator( std::size_t const block_count, std::size_t const block_size ) noexcept :
       allocator_interface( block_count * block_size ), block_count( block_count ), block_size( block_size ),
-      p_memory( new std::byte[block_count * block_size * sizeof( header )] ), p_first_free( nullptr )
+      p_memory( std::make_unique<std::byte[]>( block_count * block_size * sizeof( header ) ) ), p_first_free( nullptr )
    {
       assert( block_count != 0 && "Cannot have no blocks in memory pool" );
       assert( block_size != 0 && "Cannot have a block size of zero" );
 
-      p_first_free = reinterpret_cast<header*>( p_memory );
+      p_first_free = reinterpret_cast<header*>( p_memory.get( ) );
       auto* p_base_cpy = p_first_free;
 
       for ( int i = 1; i < block_count; ++i )
       {
          std::size_t offset = i * ( block_size + sizeof( header ) );
 
-         auto* p_new = reinterpret_cast<header*>( p_memory + offset );
+         auto* p_new = reinterpret_cast<header*>( p_memory.get( ) + offset );
          p_base_cpy->p_next = p_new;
          p_base_cpy = p_new;
          p_base_cpy->p_next = nullptr;
       }
-   }
-   pool_allocator::pool_allocator( pool_allocator&& other ) noexcept
-   {
-      *this = std::move( other );
-   }
-   pool_allocator::~pool_allocator( ) noexcept
-   {
-      block_count = 0;
-      block_size = 0;
-
-      if ( p_memory )
-      {
-         delete[] p_memory;
-         p_memory = nullptr;
-         p_first_free = nullptr;
-      }
-   }
-
-   pool_allocator& pool_allocator::operator=( pool_allocator&& rhs ) noexcept
-   {
-      if ( this != &rhs )
-      {
-         std::swap( total_size, rhs.total_size );
-         std::swap( used_memory, rhs.used_memory );
-         std::swap( num_allocations, rhs.num_allocations );
-         std::swap( block_count, rhs.block_count );
-         std::swap( block_size, rhs.block_size );
-         
-         p_memory = rhs.p_memory;
-         rhs.p_memory = nullptr;
-
-         p_first_free = rhs.p_first_free;
-         rhs.p_first_free = nullptr;
-      }
-
-      return *this;
    }
 
    std::byte* pool_allocator::allocate( std::size_t size, std::size_t alignment ) noexcept 
